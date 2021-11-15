c06e885708b6b0162c7b07123b18e2f701184a0b
Joy-El <joy-el@irisdatasolutions.co>
2021-11-15 14:03:50 -0500

    updated course_variables file, removed \r characters

---
+++ course_variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# store local variables for this course
+
+CANVAS_COURSE_ID=97249
+CANVAS_COURSE_NAME='BINF6308.Development.002139028F'
+IMAGE_URL_PREFIX='https://raw.githubusercontent.com/Joy-El/TestCurriculum/master/curriculum/'
